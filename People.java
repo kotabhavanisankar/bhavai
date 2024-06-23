3bd2e8a626ae094ccd40e521b58273ca35dc0d74
kotabhavanisankar <kotabhavanisankar@gmail.com>
2024-06-23 16:24:51 +0530

    update

---
+++ People.java
@@ -0,0 +1,15 @@
+class People
+ {
+    public static void main(String[] args)
+ {
+     int start=1;
+     for(start=1;start<1=10;start++)
+     {
+         if(start==1)
+	continue;
+         
+             System.out.println(start);
+            
+         }
+     }
+}
